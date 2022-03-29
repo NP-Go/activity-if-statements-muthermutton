4fbde91256267885dc9749310f74c48b0250a311
muthermutton <khairal.fazil@gmail.com>
2022-03-29 11:37:08 +0800

    added if comparison

--- if.go
+++ if.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-	number1 := 17
-	number2 := 24
+	// number1 := 17
+	// number2 := 24
 	resultMessage := "No outcome."
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
-
+	if number1, number2 := 17, 24; number1 < number2 {
+		resultMessage = "24 is bigger than 17"
+	}
 }
