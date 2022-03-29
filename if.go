586e3f532df9a95acf5dfe3104636b9ca651cf5e
muthermutton <khairal.fazil@gmail.com>
2022-03-29 11:50:48 +0800

    added printLn and difference calculation

--- if.go
+++ if.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// number1 := 17
 	// number2 := 24
@@ -7,6 +9,8 @@ func main() {
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
 	if number1, number2 := 17, 24; number1 < number2 {
-		resultMessage = "24 is bigger than 17"
+		resultMessage = "24 is bigger than 17 by"
+		difference := number2 - number1
+		fmt.Println(resultMessage, difference)
 	}
 }
